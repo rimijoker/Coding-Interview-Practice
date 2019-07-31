aefc477013311431f39df69134fcddf308bab283
rimijoker <anz19999999999@gmail.com>
2019-07-31 16:16:11 +0530

    New

---
+++ Revesring a linked list.py
@@ -0,0 +1,56 @@
+class ListNode(object):
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+  
+  # Function to print the list
+  def printList(self):
+    node = self
+    output = '' 
+    while node != None:
+      output += str(node.val)
+      output += " "
+      node = node.next
+    print(output)
+
+  # Iterative Solution
+  def reverseIteratively(self, head):
+    previous = None
+    current = head
+    while current:
+      nxt = current.next
+      current.next = previous
+      previous = current
+      current = nxt
+
+  def reverseRecursively(self, head):
+    # Ensures there are no cycles in the program
+    if not head or not head.next:
+      return head
+    node = self.reverseRecursively(head.next)
+    head.next.next = head
+    head.next = None
+    return node 
+    
+    
+
+# Test Program
+# Initialize the test list: 
+testHead = ListNode(4)
+node1 = ListNode(3)
+testHead.next = node1
+node2 = ListNode(2)
+node1.next = node2
+node3 = ListNode(1)
+node2.next = node3
+testTail = ListNode(0)
+node3.next = testTail
+
+print("Initial list: ")
+testHead.printList()
+# 4 3 2 1 0
+#testHead.reverseIteratively(testHead)
+testHead.reverseRecursively(testHead)
+print("List after reversal: ")
+testTail.printList()
+# 0 1 2 3 4
