33a4be22ea46d69963f8c2e999fccd45f97156bd
rimijoker <anz19999999999@gmail.com>
2020-05-17 16:15:47 +0530

    Added Algoexpert

---
+++ Algoexpert/Easy/Fibonacci Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fib(self, N: int) -> int:
+        if N <= 1:
+            return N
+        self.cache = {0: 0, 1: 1}
+        return self.memoize(N)
+
+    def memoize(self, N: int) -> {}:
+        if N in self.cache.keys():
+            return self.cache[N]
+        self.cache[N] = self.memoize(N-1) + self.memoize(N-2)
+        return self.memoize(N)
