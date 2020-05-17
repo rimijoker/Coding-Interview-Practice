f3310dcbd65388a472dd63249056cfaad95ec3a2
rimijoker <anz19999999999@gmail.com>
2020-05-17 18:08:54 +0530

    may1706082020

---
+++ Algoexpert/Easy/Two sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i in range(len(nums)):
+            if target-nums[i] in seen:
+                return[seen[target-nums[i]], i]
+            else:
+                seen[nums[i]] = i 
