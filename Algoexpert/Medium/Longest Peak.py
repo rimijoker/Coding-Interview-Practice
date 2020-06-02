b96c5a2a9b1e03aaca45f1c3cd1166fe274bd81f
rimijoker <anz19999999999@gmail.com>
2020-06-02 13:33:56 +0530

    7

---
+++ Algoexpert/Medium/Longest Peak.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestMountain(self, A: List[int]) -> int:
+        N = len(A)
+        ans = base = 0
+
+        while base < N:
+            end = base
+            
+            if end + 1 < N and A[end] < A[end + 1]: #if base is a left-boundary
+                
+                #set end to the peak of this potential mountain
+                
+                while end+1 < N and A[end] < A[end+1]:
+                    end += 1
+
+                if end + 1 < N and A[end] > A[end + 1]: #if end is really a peak..
+                
+                    #set 'end' to right-boundary of mountain
+                    
+                    while end+1 < N and A[end] > A[end+1]:
+                        end += 1
+                    
+                    #record candidate answer
+                    
+                    ans = max(ans, end - base + 1)
+
+            base = max(end, base + 1)
+
+        return ans
