5911420e76f1fa70661f2e83ffe8b05d85132439
rimijoker <anz19999999999@gmail.com>
2020-06-12 11:33:19 +0530

    15

---
+++ Algoexpert/Medium/Min Number Of Coins For Change.py
@@ -0,0 +1,19 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        minimum_number_of_coins = [0] + [float("inf")] * (amount + 1)
+
+        for coin in coins:
+            for current_amount in range(coin, amount + 1):
+                balance = current_amount - coin
+                minimum_number_of_coins[current_amount] = min(
+                    minimum_number_of_coins[current_amount],
+                    minimum_number_of_coins[balance] + 1,
+                )  # the new min may be min of coins required to make up
+                # balance(taking out the value of coin in current amount) + 1 (adding back the value of coin)
+
+        return (
+            minimum_number_of_coins[amount]
+            if minimum_number_of_coins[amount] != float("inf")
+            else -1
+        )
