4a2743b4f86ddd41a51b9bf65a1de311b6ce8e61
rimijoker <anz19999999999@gmail.com>
2020-06-12 11:27:34 +0530

    14

---
+++ Algoexpert/Medium/Number of Ways to Make Change.py
@@ -0,0 +1,8 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+
+        combo = [1] + [0] * (amount)
+        for coin in coins:
+            for current_amount in range(coin, amount + 1):
+                combo[current_amount] += combo[current_amount - coin]
+        return combo[-1]
