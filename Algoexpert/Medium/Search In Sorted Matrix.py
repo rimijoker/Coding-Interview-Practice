197f89ce56b891af902a592302b01c9c1065aa40
rimijoker <anz19999999999@gmail.com>
2020-07-02 16:38:21 +0530

    25

---
+++ Algoexpert/Medium/Search In Sorted Matrix.py
@@ -0,0 +1,13 @@
+# O(n + m)  time | O(1) space
+# where 'n' is the length of row and 'm' is the length on column
+def search_in_sorted_matrix(matrix, target):
+    row = 0
+    col = len(matrix[0]) - 1
+    while row < len(matrix) and col >= 0:
+        if matrix[row][col] > target:
+            col -= 1
+        elif matrix[row][col] < row:
+            row += 1
+        else:
+            return [row, col]
+    return [-1, -1]
