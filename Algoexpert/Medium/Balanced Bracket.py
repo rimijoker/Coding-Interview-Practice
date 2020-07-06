624a7d55c39ea2aca83041e1c241d498563f2a10
rimijoker <anz19999999999@gmail.com>
2020-07-06 11:31:49 +0530

    27

---
+++ Algoexpert/Medium/Balanced Bracket.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        # The stack to keep track of opening brackets.
+        stack = []
+
+        # Hash map for keeping track of mappings. This keeps the code very clean.
+        # Also makes adding more types of parenthesis easier
+        mapping = {")": "(", "}": "{", "]": "["}
+
+        # For every bracket in the expression.
+        for char in s:
+
+            # If the character is an closing bracket
+            if char in mapping:
+
+                # Pop the topmost element from the stack, if it is non empty
+                # The mapping for the opening bracket in our hash and the top
+                # element of the stack don't match, return False
+                if len(stack) == 0 or mapping[char] != stack.pop():
+                    return False
+            else:
+                # We have an opening bracket, simply push it onto the stack.
+                stack.append(char)
+
+        # In the end, if the stack is empty, then we have a valid expression.
+        # The stack won't be empty for cases like ((()
+        return len(stack) == 0
