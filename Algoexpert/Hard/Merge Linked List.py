214f98e48057686c5970841d936c5ee741e162e5
rimijoker <anz19999999999@gmail.com>
2020-09-21 17:44:48 +0530

    20

---
+++ Algoexpert/Hard/Merge Linked List.py
@@ -0,0 +1,27 @@
+# O(N+M) time | O(1) space
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        merged_list = current = ListNode()
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+        current.next = l1 or l2
+        return merged_list.next
