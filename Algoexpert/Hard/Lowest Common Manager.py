85c4223de154887bbd6eda4a5238091869a8d7c2
rimijoker <anz19999999999@gmail.com>
2020-09-22 20:35:24 +0530

    22

---
+++ Algoexpert/Hard/Lowest Common Manager.py
@@ -0,0 +1,29 @@
+# O(Nodes) time | O(Depth) space
+
+
+def lowestCommonManager(topManager, reportOne, reportTwo):
+    return getOrgInfo(topManager, reportOne, reportTwo).lowestCommonManager
+
+
+def getOrgInfo(manager, reportOne, reportTwo):
+    numImportantReports = 0
+    for directReport in manager.directReports:
+        orgInfo = getOrgInfo(directReport, reportOne, reportTwo)
+        if orgInfo.lowestCommonManager is not None:
+            return orgInfo
+        numImportantReports += orgInfo.numImportantReports
+    if manager == reportOne or manager == reportTwo:
+        numImportantReports += 1
+    lowestCommonManager = manager if numImportantReports == 2 else None
+    return OrgInfo(lowestCommonManager, numImportantReports)
+
+
+class Node:
+    def __init__(self, directReports):
+        self.directReports = directReports
+
+
+class OrgInfo:
+    def __init__(self, lowestCommonManager, numImportantReports):
+        self.lowestCommonManager = lowestCommonManager
+        self.numImportantReports = numImportantReports
