df142c30eba644b40a7777972e202497429b9c26
rimijoker <anz19999999999@gmail.com>
2020-09-29 08:05:41 +0530

    26

---
+++ Algoexpert/Hard/Quick Sort.py
@@ -0,0 +1,32 @@
+# O(N logN) time(average case) | O(N^2) time (worst case) | O{logN} space
+
+
+def quickSort(array):
+    quickSortHelper(array, 0, len(array) - 1)
+
+
+def quickSortHelper(array, startIdx, endIdx):
+    if startIdx > endIdx:
+        return
+    pivotIdx = startIdx
+    leftIdx = startIdx + 1
+    rightIdx = endIdx
+    while rightIdx >= leftIdx:
+        if array[leftIdx] > array[pivotIdx] and array[rightIdx] < array[pivotIdx]:
+            swap(leftIdx, rightIdx, array)
+        if array[leftIdx] <= array[pivotIdx]:
+            leftIdx += 1
+        if array[rightIdx] >= array[pivotIdx]:
+            rightIdx -= 1
+    swap(pivotIdx, rightIdx, array)
+    leftSubarrayIsSmaller = rightIdx - 1 - startIdx < endIdx - (rightIdx + 1)
+    if leftSubarrayIsSmaller:
+        quickSortHelper(array, startIdx, rightIdx - 1)
+        quickSortHelper(array, rightIdx + 1, endIdx)
+    else:
+        quickSortHelper(array, rightIdx + 1, endIdx)
+        quickSortHelper(array, startIdx, rightIdx - 1)
+
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
