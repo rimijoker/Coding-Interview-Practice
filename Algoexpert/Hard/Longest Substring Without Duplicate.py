1cff5bfc9720e669e54a66d7b701e61ae8c8472e
rimijoker <anz19999999999@gmail.com>
2020-10-04 09:20:36 +0530

    30

---
+++ Algoexpert/Hard/Longest Substring Without Duplicate.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        seen = {}
+        start = 0
+        maxLength = 0
+        for i, char in enumerate(s):
+            if char in seen:
+                start = max(start, seen[char] + 1)
+            maxLength = max(maxLength, i - start + 1)
+            seen[char] = i
+        return maxLength
