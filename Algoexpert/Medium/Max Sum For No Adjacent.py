b7ab1e4056e8e17116c1d211c69acecf00c81fa4
rimijoker <anz19999999999@gmail.com>
2020-10-11 21:44:14 +0530

    13

--- Algoexpert/Medium/Max Sum For No Adjacent.py
+++ Algoexpert/Medium/Max Sum For No Adjacent.py
@@ -1,10 +1,30 @@
-def maxSubsetSumNoAdjacent(nums):
-    inclusive, exclusive = nums[0], 0
-    for idx in range(1, len(nums)):
-        temp = inclusive
-        inclusive = max(inclusive, exclusive + nums[idx])
-        exclusive = temp
-    return inclusive
+# O(N) time | O(N) space
 
 
-print(maxSubsetSumNoAdjacent([2, 3, 10, 13, 7, 12]))
+def max_subset_sum_with_no_adjacent(array):
+    if not len(array):
+        return 0
+    if len(array) == 1:
+        return array[0]
+    max_sum = array[:]  # copying the array into max_sum
+    max_sum[1] = max(array[0], array[1])
+    for i in range(2, len(array)):
+        max_sum[i] = max_sum(max_sum[i - 1], max_sum[i - 2] + array[i])
+    return max_sum[-1]  # -1 refers to the last index
+
+
+# O(N) time | O(1) space
+
+
+def max_subset_sum_with_no_adjacent(array):
+    if not len(array):
+        return 0
+    if len(array) == 1:
+        return array[0]
+    second = array[0]
+    first = max(array[0], array[1])
+    for i in range(2, len(array)):
+        current = max(first, second + array[i])
+        second = first
+        first = current
+    return first
