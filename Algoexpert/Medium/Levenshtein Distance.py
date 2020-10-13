3ccb34f750fc0462a27185df718a0b8f766ea05b
rimijoker <anz19999999999@gmail.com>
2020-10-13 16:10:45 +0530

    16

--- Algoexpert/Medium/Levenshtein Distance.py
+++ Algoexpert/Medium/Levenshtein Distance.py
@@ -1,18 +1,26 @@
-class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        # O(n) space with rolling array
+# O(NM) time | (min(N, M)) space
 
-        l1, l2 = len(word1), len(word2)
-        previous = [i for i in range(0, l2 + 1)]
 
-        for char_in_word1 in range(1, l1 + 1):
-            current = [char_in_word1] * (l2 + 1)
-            for char_in_word2 in range(1, l2 + 1):
-                current[char_in_word2] = min(
-                    current[char_in_word2 - 1] + 1,
-                    previous[char_in_word2 - 1] + (word1[char_in_word1 - 1] != word2[char_in_word2 - 1]),
-                    previous[char_in_word2] + 1,
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        word1_length = len(word1)
+        word2_length = len(word2)
+
+        previous = [i for i in range(word1_length + 1)]
+
+        for word2Idx in range(1, word2_length + 1):
+            current = [word2Idx] * (word1_length + 1)
+            for word1Idx in range(1, word1_length + 1):
+                isEqual = 0 if word1[word1Idx - 1] == word2[word2Idx - 1] else 1
+                current[word1Idx] = min(
+                    previous[word1Idx] + 1,
+                    current[word1Idx - 1] + 1,
+                    previous[word1Idx - 1] + isEqual,
                 )
             previous = current[:]
-
         return previous[-1]
