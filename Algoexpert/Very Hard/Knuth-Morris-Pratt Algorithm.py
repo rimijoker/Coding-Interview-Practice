35b6124651c93f97a8fece8ec67bee83e1874995
rimijoker <anz19999999999@gmail.com>
2020-10-24 18:51:04 +0530

    10

---
+++ Algoexpert/Very Hard/Knuth-Morris-Pratt Algorithm.py
@@ -0,0 +1,38 @@
+# O(N+M) time | O(M) space
+
+
+def kunthMorrisPrattAlgorithm(string, substring):
+    pattern = buildPattern(substring)
+    return doesMatch(string, substring, pattern)
+
+
+def buildPattern(substring):
+    pattern = [-1 for _ in substring]
+    j = 0
+    i = 1
+    while i < len(substring):
+        if substring[i] == substring[j]:
+            pattern[i] = j
+            i += 1
+            j += 1
+        elif j > 0:
+            j = pattern[j - 1] + 1
+        else:
+            i += 1
+    return pattern
+
+
+def doesMatch(string, substring, pattern):
+    i = 0
+    j = 0
+    while i + len(substring) - j <= len(string):
+        if string[i] == string[j]:
+            if j == len(substring) - 1:
+                return True
+            i += 1
+            j += 1
+        elif j > 0:
+            j = pattern[j - 1] + 1
+        else:
+            i += 1
+    return False
